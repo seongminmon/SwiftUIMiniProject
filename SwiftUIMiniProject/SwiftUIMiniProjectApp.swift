0b34202312515fbcb7193d277eb990bb0ca5e1ba
seongmin kim <k2417000@naver.com>
2024-09-07 21:38:41 +0900

    Initial Commit

---
+++ SwiftUIMiniProject/SwiftUIMiniProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMiniProjectApp.swift
+//  SwiftUIMiniProject
+//
+//  Created by 김성민 on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMiniProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
