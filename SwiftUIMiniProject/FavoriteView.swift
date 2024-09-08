46e8fb9d52277750e7eac0f9c96b21c7f782eb76
seongmin kim <k2417000@naver.com>
2024-09-08 17:34:19 +0900

    즐겨찾기 View 그리기

--- SwiftUIMiniProject/FavoriteView.swift
+++ SwiftUIMiniProject/FavoriteView.swift
@@ -8,11 +8,66 @@
 import SwiftUI
 
 struct FavoriteView: View {
+    
+    private let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text("FavoriteView")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(0..<5) { item in
+                        favoriteCell()
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Favorite Coin")
+        }
+    }
+    
+    func favoriteCell() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(.white)
+                .shadow(color: .black.opacity(0.1), radius: 25.0)
+            
+            VStack {
+                HStack {
+                    Image(systemName: "star")
+                        .frame(width: 50, height: 50)
+                        .background(.orange)
+                        .clipShape(Circle())
+                    VStack {
+                        Text("Bitcoin")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text("BTC")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                Spacer(minLength: 40)
+                VStack {
+                    Text("$13,235,425")
+                        .font(.title3)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                    Text("+0.64%")
+                        .foregroundStyle(.red)
+                        .padding(8)
+                        .background(
+                            RoundedRectangle(cornerRadius: 10.0)
+                                .fill(.red.opacity(0.2))
+                        )
+                        .lineLimit(1)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+            }
+            .bold()
+            .padding()
+        }
     }
 }
 
-//#Preview {
-//    FavoriteView()
-//}
+#Preview {
+    FavoriteView()
+}
