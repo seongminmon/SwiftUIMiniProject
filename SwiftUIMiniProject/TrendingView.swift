703f85493536a00506946b0db282482e954fe051
seongmin kim <k2417000@naver.com>
2024-09-08 15:39:05 +0900

    top15CoinSection 통신 -> 데이터 뷰에 그리기

--- SwiftUIMiniProject/TrendingView.swift
+++ SwiftUIMiniProject/TrendingView.swift
@@ -7,17 +7,93 @@
 
 import SwiftUI
 
+struct CoinItem: Hashable {
+    let id: String
+    let name: String
+    let symbol: String
+    let small: String?
+    let price: Double?
+    let priceChange: Double?
+    
+    var priceFormatted: String {
+        if let price {
+            let formatted = String(format: "%.2f", price)
+            return "₩\(formatted)"
+        } else {
+            return "n\\a"
+        }
+    }
+    
+    var priceChangeFormatted: String {
+        if let priceChange {
+            let formatted = String(format: "%.2f", priceChange)
+            return "\(formatted)%"
+        } else {
+            return "n\\a"
+        }
+    }
+}
+
+//struct NFTItem: Hashable, Identifiable {
+//    let id = UUID()
+//    let name: String
+//    let symbol: String
+//    let thumb: String?
+//    let price: Double?
+//    let priceChange: Double?
+//    
+//    var priceFormatted: String {
+//        if let price {
+//            let formatted = String(format: "%.2f", price)
+//            return "₩\(formatted)"
+//        } else {
+//            return "n\\a"
+//        }
+//    }
+//    
+//    var priceChangeFormatted: String {
+//        if let priceChange {
+//            let formatted = String(format: "%.2f", priceChange)
+//            return "\(formatted)%"
+//        } else {
+//            return "n\\a"
+//        }
+//    }
+//}
+
 struct TrendingView: View {
+    
+    // TODO: - 즐겨찾기 -> Realm -> ids -> Market 통신 -> data 세팅
+    // TODO: - Trending -> coinList / nftList
+    
+    let rows = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
+    
+    @State private var coinList = [CoinItem]()
+//    @State private var nftList = [NFTItem]()
     
     var body: some View {
         NavigationView {
             ScrollView {
                 favoriteSection()
-                
                 top15CoinSection()
                 top7NFTSection()
             }
             .navigationTitle("Crypto Coin")
+        }
+        .task {
+            CoingeckoAPIManager.shared.fetchTrending { trending in
+                coinList = trending.coins.map {
+                    let item = $0.item
+                    return CoinItem(
+                        id: item.id,
+                        name: item.name,
+                        symbol: item.symbol,
+                        small: item.small,
+                        price: item.data?.price,
+                        priceChange: item.data?.priceChangePercentage24H["krw"]
+                    )
+                }
+            }
         }
     }
     
@@ -79,45 +155,58 @@ struct TrendingView: View {
             Text("Top15 Coin")
                 .font(.title2)
                 .bold()
-                .padding(.horizontal)
             
             ScrollView(.horizontal) {
-                LazyHStack {
-                    ForEach(0..<5) { i in
-                        VStack {
-                            ForEach(0..<3) { j in
-                                coinCell()
-                            }
-                        }
+                LazyHGrid(rows: rows) {
+                    ForEach(Array(zip(coinList.indices, coinList)), id: \.0) { index, item in
+                        coinCell(index, item)
                     }
                 }
             }
             .scrollIndicators(.hidden)
         }
+        .frame(maxWidth: .infinity)
+        .padding()
     }
     
-    func coinCell() -> some View {
+    func coinCell(_ index: Int, _ item: CoinItem) -> some View {
         VStack {
             HStack {
-                Text("\(1)")
+                Text("\(index + 1)")
                     .font(.title2)
                     .bold()
-                Image(systemName: "star")
-                    .frame(width: 50, height: 50)
-                    .background(.orange)
-                    .clipShape(Circle())
+                AsyncImage(url: URL(string: item.small ?? "")) { data in
+                    switch data {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                    case .failure(_):
+                        Image(systemName: "star")
+                    @unknown default:
+                        Image(systemName: "star")
+                    }
+                }
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                
                 VStack(alignment: .leading) {
-                    Text("Solana")
+                    Text(item.name)
                         .bold()
-                    Text("LTC")
+                    Text(item.symbol)
                 }
                 Spacer()
                 
                 VStack(alignment: .trailing) {
-                    Text("$0.4135")
+                    Text(item.priceFormatted)
                         .bold()
-                    Text("+21.18%")
-                        .foregroundStyle(.red)
+                    if let change = item.priceChange, change >= 0 {
+                        Text("+\(item.priceChangeFormatted)")
+                            .foregroundStyle(.red)
+                    } else {
+                        Text("\(item.priceChangeFormatted)")
+                            .foregroundStyle(.blue)
+                    }
                 }
             }
             
@@ -135,7 +224,6 @@ struct TrendingView: View {
             Text("Top7 NFT")
                 .font(.title2)
                 .bold()
-                .padding(.horizontal)
             
             ScrollView(.horizontal) {
                 LazyHStack {
@@ -150,6 +238,8 @@ struct TrendingView: View {
             }
             .scrollIndicators(.hidden)
         }
+        .frame(maxWidth: .infinity)
+        .padding()
     }
     
     func nftCell() -> some View {
@@ -187,6 +277,6 @@ struct TrendingView: View {
     }
 }
 
-#Preview {
-    TrendingView()
-}
+//#Preview {
+//    TrendingView()
+//}
